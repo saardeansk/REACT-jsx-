a48743edc0caeb46e06c7a62f05b27d996e137fc
SATHISHKUMAR.S <sathishkumarsk2210@gmail.com>
2026-01-29 23:27:48 +0530

    Create app.jsx

---
+++ app.jsx
@@ -0,0 +1,19 @@
+
+import './App.css'
+import Sathish from './Sathish.jsx';
+import Footer from './check.jsx';
+function App() {
+
+  return (
+      <div>
+    <h1> hai da i ma  sathish from salem </h1>
+   <>
+    <Sathish></Sathish> 
+    <Footer></Footer>
+    </>
+    </div>
+
+  );
+}
+
+export default App;
