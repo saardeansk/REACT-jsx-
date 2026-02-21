1963a16e80033ff5c203237022915ee64980ad2c
SATHISHKUMAR.S <sathishkumarsk2210@gmail.com>
2026-02-21 22:03:36 +0530

    Update app.jsx

--- app.jsx
+++ app.jsx
@@ -3,7 +3,7 @@ import './App.css'
 import Sathish from './Sathish.jsx';
 import Footer from './check.jsx';
 function App() {
-
+  
   return (  
       <div>
     <h1> hai da i ma  sathish from salem </h1>
